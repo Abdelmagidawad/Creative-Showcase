c6716dc461a2720f3939d884dc00c22603eed62b
Abdelmagidawad <abdelmagidawad@gmail.com>
2024-08-01 18:23:34 +0300

    add section stats to increase numbers on scroll

--- migo.js
+++ migo.js
@@ -1,3 +1,28 @@
+// section stats
+let sectionStats = document.querySelector(".stats");
+let numsStats = document.querySelectorAll(".stats .number");
+let started = false; //Function Started ? No
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= sectionStats.offsetTop) {
+    if (!started) {
+      numsStats.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+});
+
+function startCount(element) {
+  let goal = element.dataset.goal;
+  let count = setInterval(() => {
+    // let num = parseInt(element.textContent);
+    // num++;
+    element.textContent++;
+    // if (num === goal) clearInterval(count);
+    if (element.textContent === goal) clearInterval(count);
+  }, 2000 / goal);
+}
+
+// Icon Scroll
 let iconScroll = document.querySelector(".UP");
 
 window.addEventListener("scroll", function () {
