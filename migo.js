f5e8c08fd4a3ac4b16450c701604757149f2536d
Abdelmagidawad <abdelmagidawad@gmail.com>
2024-11-28 01:08:06 +0200

    add section events countdown time

--- migo.js
+++ migo.js
@@ -10,6 +10,28 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Section events
+let countDown = new Date("Dec 31, 2024 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+  let dateDiff = countDown - dateNow;
+
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) clearInterval(counter);
+}, 1000);
+
 // section stats
 let sectionStats = document.querySelector(".stats");
 let numbers = document.querySelectorAll(".stats .number");
@@ -34,7 +56,6 @@ function startCount(element) {
     }
   }, 2000 / goal);
 }
-
 // Icon Scroll
 let iconScroll = document.querySelector(".UP");
 
